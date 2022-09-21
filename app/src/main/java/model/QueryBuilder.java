045ee5ef50f14c8ef314d84d97ca38899f2995c6
Matteo <matteo.iorio01@gmail.com>
2022-09-21 14:17:19 +0200

    Extract interface

--- app/src/main/java/model/QueryBuilder.java
+++ app/src/main/java/model/QueryBuilder.java
@@ -1,6 +1,6 @@
 package model;
 
-public class QueryBuilder {
+public class QueryBuilder implements Builder {
 	private final static String API = "https://api.minerstat.com/v2/coins?";
 	private String list ="list=";
 	
@@ -8,7 +8,8 @@ public class QueryBuilder {
 		
 	}
 	
-	public QueryBuilder selectCoin(final Coin coin) {
+	@Override
+	public Builder selectCoin(final Coin coin) {
 		if(this.list.equals("list=")) {
 			this.list = String.join(this.list, coin.getName());
 		}else {
@@ -18,13 +19,15 @@ public class QueryBuilder {
 		return this;
 	}
 	
-	public QueryBuilder peekRandom() {
+	@Override
+	public Builder peekRandom() {
 		/*Select a random coin*/
 		/*If in list != "list=" throw exception */
 		/*Add the random coin at the query */
 		return this;
 	}
 	
+	@Override
 	public String build() {
 		
 		return String.join(API, list);

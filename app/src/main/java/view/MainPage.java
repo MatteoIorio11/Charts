159866daf9c6f90c7e9bd702b24c09bfe2e6d0c5
Matteo <matteo.iorio01@gmail.com>
2022-09-21 17:18:55 +0200

    Init single coin page

--- app/src/main/java/view/MainPage.java
+++ app/src/main/java/view/MainPage.java
@@ -1,5 +1,16 @@
 package view;
 
-public class MainPage {
+import controller.Controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
+public class MainPage {
+	@FXML private Button singleCoint;
+	@FXML private Button twoCoints;
+	
+	private final Controller controller;
+	
+	public MainPage(final Controller controller) {
+		this.controller = controller;
+	}
 }
--- app/src/main/java/view/SingleCoin.java
+++ app/src/main/java/view/SingleCoin.java
@@ -4,14 +4,20 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import controller.Controller;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.AreaChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.ChoiceBox;
+import model.Coin;
 
-public class SingleCoin implements Initializable{
-	@FXML private ChoiceBox coins;
-	@FXML private AreaChart areaChart;
+
+
+public class SingleCoin extends Thread implements Initializable{
+	@FXML private ChoiceBox<Coin> coins;
+	@FXML private AreaChart<Integer, Double> areaChart;
+	private final static long SECONDS = 5000;
 	private final Controller controller;
 	public SingleCoin(final Controller controller) {
 		this.controller = controller;
@@ -19,11 +25,32 @@ public class SingleCoin implements Initializable{
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
-		
+		this.coins.getItems().addAll(Coin.values());
 	}
 	
 	
+	/*
+	@Override
+	@FXML
+	public void selectCoinHandler(final ActionEvent action) {
+		this.displayChart(this.coins.getValue());
+	}
+	*/
 	
-	
+	private void displayChart(final Coin selectedCoin) {
+		final XYChart.Series<Integer, Double> serie = new XYChart.Series<>();
+		serie.setName(selectedCoin.getName());
+			try {
+				while(true) {
+					this.controller.getPrice(selectedCoin).stream()
+						.forEach((price) -> 
+							serie.getData().add(
+									new XYChart.Data<Integer, Double>(price.getKey(), price.getValue())));
+					SingleCoin.sleep(SingleCoin.SECONDS);
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+	}
 }
+

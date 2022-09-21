45686d33c0bcde379cd4fde24d214b68200706ff
Matteo <matteo.iorio01@gmail.com>
2022-09-21 14:06:38 +0200

    correction in SelectCoint

--- app/src/main/java/model/QueryBuilder.java
+++ app/src/main/java/model/QueryBuilder.java
@@ -3,7 +3,6 @@ package model;
 public class QueryBuilder {
 	private final static String API = "https://api.minerstat.com/v2/coins?";
 	private String list ="list=";
-	private String qeury = "";
 	
 	public QueryBuilder() {
 		
@@ -21,6 +20,7 @@ public class QueryBuilder {
 	
 	public QueryBuilder peekRandom() {
 		/*Select a random coin*/
+		/*If in list != "list=" throw exception */
 		/*Add the random coin at the query */
 		return this;
 	}

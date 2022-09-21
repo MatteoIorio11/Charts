90be8e76808a3c6c938017f611aee3e26a0b7dc6
Fabio Vincenzi <fabio.vincenzi2@studio.unibo.it>
2022-09-21 17:53:45 +0200

    creating main page button handler

--- app/src/main/java/view/MainPage.java
+++ app/src/main/java/view/MainPage.java
@@ -5,38 +5,61 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import controller.Controller;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
 
 public class MainPage implements Initializable{
-	@FXML private BorderPane mainPane;
-	@FXML private Button singleCoint;
-	@FXML private Button twoCoints;
+	@FXML private Button singleCoin;
+	@FXML private Button twoCoins;
 	
 	private final Controller controller;
 	
 	public MainPage(final Controller controller) {
 		this.controller = controller;
 	}
-	private <X> void loadPage(final Page page, final X controller) {
-        Pane pane;
-        try {
-            pane = Loader.loadPane(page.getPath(), controller);
-            this.mainPane.setCenter(pane);
-        } catch (IOException e) {
-            final Alert errorMessage = new Alert(AlertType.ERROR);
-            errorMessage.setContentText(e.getMessage());
-            errorMessage.showAndWait();
-        }
+
+	
+	public static <X> Stage loadStage(final String path, final String title, final X controller, final double minHeight, final double minWidth) throws IOException {
+        final Pane pane;
+        final Rectangle2D screenBounds = Screen.getPrimary().getBounds();
+        final double xSize =  screenBounds.getMaxX() /	 2;
+        final double ySize = screenBounds.getMaxY() / 2;
+        final Stage newWindow = new Stage();
+        newWindow.setMinHeight(minHeight);
+        newWindow.setMinWidth(minWidth);
+        final var loader = new FXMLLoader(Loader.class.getResource(path));
+        loader.setController(controller);
+
+        pane = loader.load();
+
+        final Scene secondScene = new Scene(pane, xSize, ySize);
+        newWindow.setTitle(title);
+        newWindow.setScene(secondScene);
+        return newWindow;
     }
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		
 		
 	}
+	
+    @FXML
+    public void btnSingleCoin(final ActionEvent event) throws IOException {
+        this.loadStage(Page.SINGLE_COIN.getPath(), "SingleCoin", this.controller, 100, 100);
+    }
+    
+    @FXML
+    public void btnTwoCoins(final ActionEvent event) throws IOException {
+        this.loadStage(Page.TWO_COINS.getPath(), "TwoCoins", this.controller, 100, 100);
+    }
 }

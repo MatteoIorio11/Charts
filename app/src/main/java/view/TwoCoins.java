065f37705788051598b2fca53478a540fd5d3b4c
Matteo <matteo.iorio01@gmail.com>
2022-09-22 17:35:46 +0200

    update page

--- app/src/main/java/view/TwoCoins.java
+++ app/src/main/java/view/TwoCoins.java
@@ -39,32 +39,12 @@ public class TwoCoins implements Initializable{
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		this.areaChart.setAnimated(false);
-		this.areaChart.getData().add(serie1);
-		this.areaChart.getData().add(serie2);
-		this.firstCoin.getItems().addAll(Coin.values());
-		this.secondCoin.getItems().addAll(Coin.values());
-		
-		this.firstCoin.setOnAction((e) -> {
-			this.coin1 = this.firstCoin.getSelectionModel().getSelectedItem();
-       });
-		this.secondCoin.setOnAction((e) -> {
-			this.coin2 = this.secondCoin.getSelectionModel().getSelectedItem();
-       });
+
 	}
 	
 	@FXML
 	public void launchHandler(final ActionEvent action) {
-		 ScheduledExecutorService scheduledExecutorService;
-	        scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
-		 scheduledExecutorService.scheduleAtFixedRate(() -> {
-	            Platform.runLater(() -> {
-	            	serie1.getData().add(new XYChart.Data<Integer,Double>(numberOfPoints++, controller.getPrice(coin1)));
-	                serie2.getData().add(new XYChart.Data<Integer,Double>(numberOfPoints++, controller.getPrice(coin2)));
-	            });
-	        }, 0, 1, TimeUnit.SECONDS);
-		
-		 this.launch.setVisible(false);
+
 	}
 	
 
